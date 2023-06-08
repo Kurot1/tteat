f408dfa7fe7e62627bafa3fb6af3ab63b5fc4918
jiyoungLee <kurotsuno365@gmail.com>
2023-06-08 12:03:53 +0900

    comlete?

--- pubtest.py
+++ pubtest.py
@@ -1,38 +1,15 @@
 import paho.mqtt.client as mqtt
-import RPi.GPIO as GPIO
-import json
 import time
+import json
+import adafruit_dht
 import psutil
 
 for proc in psutil.process_iter():
     if proc.name() == 'libgpiod_pulsein':
         proc.kill()
         
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
+dht_device = adafruit_dht.DHT22(4)
 
-buzzer = 12
-
-
-LED = 23
-button = 24
-TRIG = 19
-ECHO = 13
-
-GPIO.setup(buzzer, GPIO.OUT)
-
-print("start")
-
-GPIO.setup(TRIG, GPIO.OUT)
-GPIO.setup(ECHO, GPIO.IN)
-
-GPIO.setup(LED,GPIO.OUT)
-GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
-def my_callback(channel):
-    print("버튼이 눌렸습니다")
-    
-GPIO.add_event_detect(button, GPIO.RISING, callback=my_callback)
 
 MQTT_HOST = "broker.emqx.io"
 MQTT_PORT = 1883
@@ -43,62 +20,23 @@ client = mqtt.Client()
 client.connect(MQTT_HOST,MQTT_PORT,MQTT_KEEPALSE_INTERVAL )
 client.loop_start()
 
-
-
-
 try:
-    p = GPIO.PWM(buzzer, 262)
-    num = 1
     while True:
-        #버튼 감지
-        print(num)
-        time.sleep(1)
-        num =num+1
-        
-        #거리 감지 
-        GPIO.output(TRIG, False)
-        time.sleep(0.5)
-
-        GPIO.output(TRIG, True)
-        time.sleep(0.00001)
-        GPIO.output(TRIG, False)
-
-        while GPIO.input(ECHO) == 0 :
-            pulse_start = time.time()
-
-        while GPIO.input(ECHO) == 1 :
-            pulse_end = time.time()
-            
-        pulse_duration = pulse_end - pulse_start
-        distance = pulse_duration * 17000
-        distance = round(distance, 2)
-
-        print("거리 : ", distance, "cm")
-        
-        if distance >= 30:
-            print("물건이 범위 밖에 있습니다")
-            GPIO.output(LED, GPIO.HIGH)
-            value = json.dumps(sensing)
+        try:
+            time.sleep(5)
+            #dht sensor
+            humidity, temperature =80, 30
+            distance = {
+                "temperation":temperature,
+                "humidity": humidity
+              }
+            value = json.dumps(distance)
             client.publish(MQTT_PUB_TOPIC,value)
             print(value)
-            p.start(50)
-            time.sleep(1)
-            p.stop()
-        else:
-            print("물건이 범위 안에 있습니다")
-            GPIO.output(LED, GPIO.LOW)
-            time.sleep(1)
-            
-            
-        
-
-    
-    
-        
-
-        
+        except RuntimeError:
+            time.sleep(5)
+            continue
 except KeyboardInterrupt:
-    print("종료합니다!!")
+    print("I'm done!!")
 finally:
-    GPIO.cleanup()
-   
+    client.disconnect()
--- teat.py
+++ teat.py
@@ -15,7 +15,7 @@ buzzer = 12
 
 
 LED = 23
-button = 24
+
 TRIG = 19
 ECHO = 13
 
@@ -27,12 +27,9 @@ GPIO.setup(TRIG, GPIO.OUT)
 GPIO.setup(ECHO, GPIO.IN)
 
 GPIO.setup(LED,GPIO.OUT)
-GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-def my_callback(channel):
-    print("버튼이 눌렸습니다")
-    
-GPIO.add_event_detect(button, GPIO.RISING, callback=my_callback)
+
+
 
 MQTT_HOST = "broker.emqx.io"
 MQTT_PORT = 1883
@@ -48,13 +45,7 @@ client.loop_start()
 
 try:
     p = GPIO.PWM(buzzer, 262)
-    num = 1
     while True:
-        #버튼 감지
-        print(num)
-        time.sleep(1)
-        num =num+1
-        
         #거리 감지 
         GPIO.output(TRIG, False)
         time.sleep(0.5)
@@ -74,29 +65,44 @@ try:
         distance = round(distance, 2)
 
         print("거리 : ", distance, "cm")
-        
+        #물건이 범위 밖에 있을때
         if distance >= 30:
             print("물건이 범위 밖에 있습니다")
             GPIO.output(LED, GPIO.HIGH)
             p.start(50)
             time.sleep(1)
             p.stop()
+            
+            try:
+                sensing = {"distance":distance}
+                value = json.dumps(sensing)
+                client.publish(MQTT_PUB_TOPIC,value)
+                print(value)
+            except RuntimeError:
+                time.sleep(5)
+                continue
+          #물건이 범위 안에 있을때 
         else:
             print("물건이 범위 안에 있습니다")
             GPIO.output(LED, GPIO.LOW)
             time.sleep(1)
+            try:
+                safe = {"distance":distance}
+                value = json.dumps(safe)
+                client.publish(MQTT_PUB_TOPIC,value)
+                print(value)
+            except RuntimeError:
+                time.sleep(5)
+                continue
+            
+           
             
             
-        
-
-    
-    
-        
-
-        
+           
 except KeyboardInterrupt:
     print("종료합니다!!")
 finally:
     GPIO.cleanup()
+    client.disconnect()
    
 
